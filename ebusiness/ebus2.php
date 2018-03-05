ebd2cbf2ab9a667a4c65b334630c7caf452f6509
Cian O Mahony <117493954@umail.ucc.ie>
2018-03-05 19:59:25 +0000

    adding meta tags and giving the correct titles

--- ebusiness/ebus2.php
+++ ebusiness/ebus2.php
@@ -10,7 +10,10 @@ session_start();
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="ebus2_validator.js"></script>
         
-        <title>Select Product</title>
+        <title>Enter Details</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
