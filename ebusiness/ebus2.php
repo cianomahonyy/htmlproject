dc04256cf095d94adee9cb960b8bfbdee8c582bf
Cian O Mahony <117493954@umail.ucc.ie>
2018-03-05 19:52:10 +0000

    indenting code

--- ebusiness/ebus2.php
+++ ebusiness/ebus2.php
@@ -22,76 +22,58 @@ session_start();
         
         
     <ul class="nav">
-
       <div class="logo">
           <img src="../images/cloudiologo.png" id="cloudiologo" href="cloudhome.html"/></li>
       </div>
-
       <li><a href="cloudhome.html">Home</a></li>
       <li><a href="aboutcloud.html">About Cloud</a></li>
       <li><a href="ebus1.php">Products</a></li>
       <li><a href="contactus.html">Contact Us</a></li>
-
     </ul>
         
         
         
         
         
-        <div align="center" class="addtocart-wrapper">
-        
-        <h4 id="paymentdetails">Enter Your Payment Details</h4>
-        
-        
-        <form action="ebus3.php" method="POST">
-
+    <div align="center" class="addtocart-wrapper">
+      <h4 id="paymentdetails">Enter Your Payment Details</h4>
+      
+      <form action="ebus3.php" method="POST">
           <label for="name">
-
             <input type="text" id="name" name="name" placeholder="Name" value=""/>
-
           </label>
 
           </br>
 
           <label for="email">
-
             <input type="text" id="email" name="email" placeholder="Email" value=""/>
-
           </label>
 
           </br>
 
           <label for="user_pin">
-                    
             <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
-                    
           </label>
 
           </br>
-            
-          
-          
           </br>
             
-            <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
-              
-          </form>
+          <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
+      </form>
             
-          <br/>
-            
-            
-        <button id="btnValidate" onClick="validateDetails()" enabled>Validate</button>
-          
-            
-        </div>
+      <br/>
+      
+      <button id="btnValidate" onClick="validateDetails()" enabled>Validate</button>
+    </div>
         
-        <?php
-        // Set session variables
-        $_SESSION["total"] = $_POST["total"];
-        $_SESSION["vat"] = $_POST["vat"];
-        $_SESSION["discount"] = $_POST["discount"];
-        $_SESSION["subtotal"] = $_POST["subtotal"];
-        ?>
+    <?php
+      // Set session variables
+      $_SESSION["total"] = $_POST["total"];
+      $_SESSION["vat"] = $_POST["vat"];
+      $_SESSION["discount"] = $_POST["discount"];
+      $_SESSION["subtotal"] = $_POST["subtotal"];
+    ?>
+        
         
     </body>
 </html>
--- ebusiness/ebus3.php
+++ ebusiness/ebus3.php
@@ -4,30 +4,34 @@ session_start();
 <!DOCTYPE html>
 <html>
     <head>
-        <title>RECEIPT</title>
+        <title>Thank You For Your Order!</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="cost_calc.js"></script>
         <link rel="stylesheet" href="../css/cloudstyle.css" type="text/css" />
     </head>
+    
     <body>
         
+        
     <ul class="nav">
-
       <div class="logo">
           <img src="../images/cloudiologo.png" id="cloudiologo" href="cloudhome.html"/></li>
       </div>
-
       <li><a href="cloudhome.html">Home</a></li>
       <li><a href="aboutcloud.html">About Cloud</a></li>
       <li><a href="ebus1.php">Products</a></li>
       <li><a href="contactus.html">Contact Us</a></li>
-
     </ul>
         
         
         
     <h4 align="center" id="receipt">Receipt</h4>
+            
                 
         <?php
         $_SESSION["name"] = $_POST["name"];
@@ -35,24 +39,26 @@ session_start();
         
         echo "<div align='center' class='receiptwrapper'>";
         
-        echo "<h4 id='receiptname'>Name: " . $_SESSION['name'] . "</h4>";
-        echo "<h4 id='receiptemail'>Email: " . $_SESSION['email'] . "</h4>";
+            echo "<h4 id='receiptname'>Name: " . $_SESSION['name'] . "</h4>";
+            echo "<h4 id='receiptemail'>Email: " . $_SESSION['email'] . "</h4>";
         
-        echo "<div align='center' class='receiptpricewrapper'>";
+            echo "<div align='center' class='receiptpricewrapper'>";
         
-        echo "<h4 align='center' id='prouctname'>Salesforce</h4>";
+                echo "<h4 align='center' id='prouctname'>Salesforce</h4>";
         
-        echo "<h4 id='receiptprice'>Subtotal: " . $_SESSION['subtotal'] . "</h4>";
-        echo "<h4 id='receiptprice'>VAT: " . $_SESSION['vat'] . "</h4>";
-        echo "<h4 id='receiptprice'>Discount: " . $_SESSION['discount'] . "</h4>";
-        echo "<h4 id='receiptprice'>Total: " . $_SESSION['total'] . "</h4>";
+                echo "<h4 id='receiptprice'>Subtotal: " . $_SESSION['subtotal'] . "</h4>";
+                echo "<h4 id='receiptprice'>VAT: " . $_SESSION['vat'] . "</h4>";
+                echo "<h4 id='receiptprice'>Discount: " . $_SESSION['discount'] . "</h4>";
+                echo "<h4 id='receiptprice'>Total: " . $_SESSION['total'] . "</h4>";
         
-        echo "</div>";
+            echo "</div>";
         
-        echo "<h2 id='receiptthankyou'>Thank You For Your Order!</h2>";
+            echo "<h2 id='receiptthankyou'>Thank You For Your Order!</h2>";
         
         echo "</div>";
         
         ?>
+        
+        
     </body>
 </html>
